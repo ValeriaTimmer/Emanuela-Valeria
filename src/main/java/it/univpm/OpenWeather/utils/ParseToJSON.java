f878d69f5b0b266f95821cabd3482dbe4d1f862c
EmanuelaSaleggia <75066510+EmanuelaSaleggia@users.noreply.github.com>
2020-12-19 15:47:00 +0100

    Update ParseToJSON.java

--- src/main/java/it/univpm/OpenWeather/utils/ParseToJSON.java
+++ src/main/java/it/univpm/OpenWeather/utils/ParseToJSON.java
@@ -1,6 +1,15 @@
 package it.univpm.OpenWeather.utils;
 
+import it.univpm.OpenWeather.service.*;
+import it.univpm.OpenWeather.model.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ParseToJSON {
+	
+	private ArrayList<City> città;
+	private Iteratori<City> it;
 	
 	public ParseToJSON() {}
 	
