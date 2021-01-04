a7a2be31a3f02f571205d1561653757510ee48a2
Emanuela <Emanuela@LAPTOP-0GK5BIP8>
2021-01-04 22:33:06 +0100

    modifica provvisoria

--- src/main/java/it/univpm/OpenWeather/statistics/Stats.java
+++ src/main/java/it/univpm/OpenWeather/statistics/Stats.java
@@ -4,9 +4,14 @@ import org.json.simple.JSONArray;
 import java.text.ParseException;
 import it.univpm.OpenWeather.utils.StatsUtils;
 import it.univpm.OpenWeather.exception.*;
+import java.util.Vector;
+import java.util.Date;
+import java.util.Locale;
+import java.text.DateFormat;
+
 
 /**
- * Interfaccia per le statistiche
+ * Classe per le statistiche
  * @author Valeria Timmer
  * @author Emanuela Saleggia
  *
@@ -17,6 +22,19 @@ public class Stats <T, P, Q> {
 	 * Array contenente le città
 	 */
      private JSONArray arrayCity;
+     
+     private static Date dateFrom;
+     
+     private static Date today;
+     
+     /**
+ 	 *  DateFormat per il parsing della data in formato: "dd-MM-yyyy"
+ 	 */
+ 	 private static DateFormat formatoData = DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
+ 	 
+ 	 String datainiziale = formatoData.format(dateFrom);
+ 	 
+ 	 String data_di_oggi = formatoData.format(today);
      
      /**
  	 * Variabile utilizzata per richiamare il metodo della classe StatsUtilsTemperature
@@ -39,6 +57,46 @@ public class Stats <T, P, Q> {
  	 public JSONArray getArray() {
  		return arrayCity;
  	}
+ 	 
+ 	public static long getPeriod (String from, String to) throws DataFormatException, ParseException {
+		if (today.before(dateFrom)) 
+			throw new DataFormatException();
+		return ((today.getTime()- dateFrom.getTime()) / (24*60*60*1000));
+	}
+ 	
+ 	/**
+	 * Metodo che inserisce in un JSONArray le date comprese tra 
+	 * la data di inizio e la data di fine
+	 * @param from Data iniziale
+	 * @param to Data finale
+	 * @return d Vettore contenente le date
+	 * @throws DataFormatException Eccezione personalizzata
+	 * @throws ParseException Errore di parsing
+	 */
+	public static JSONArray date (String from, String to) throws DataFormatException, ParseException{
+		JSONArray d = new JSONArray();
+		Long period = getPeriod(from, to);
+		for (int i=0; i<=period; i++) {
+			d.add(formatoData.format(dateFrom));
+			dateFrom.setTime(dateFrom.getTime()+(24*60*60*1000));
+		}
+		return d;
+	}
+ 	
+ 	/**
+ 	 * metodo che verifica le previsioni
+ 	 * @return
+ 	 */
+ 	public boolean verificaPrevisioni( Double humidity, Double temperature, String from, String to)throws DataFormatException, ParseException {
+ 		JSONArray d1 = date(datainiziale, data_di_oggi);
+ 		for(Object ob: d1) {
+ 			if(ob instanceof Object)
+ 			 if(humidity.equals(today)&& temperature.equals(today))
+ 				return true;}
+ 		      return false;
+ 	}
+ 	
+ 	 
 	/**
 	* Metodo astratto che viene implementato a seconda dell'esigenza
 	* @param arrayCity Array contenente le città
